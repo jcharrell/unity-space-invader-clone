83f227cc6b8ff810e46b7e886602e5a754dd1577
Chris Harrell <chris.harrell@sq1.com>
2017-03-24 17:42:55 -0500

    Imported starting menu component

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name) {
+		Application.LoadLevel (name);
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
