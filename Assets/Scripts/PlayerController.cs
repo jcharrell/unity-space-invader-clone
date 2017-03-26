19c3c5d8c1f7f4d440420a33c6adf75f3236c51c
Chris Harrell <chris.harrell@sq1.com>
2017-03-25 20:27:05 -0500

    Restrict player to the gamespace

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
 	
+	float xMin = -5.0f;
+	float xMax = 5.0f;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -13,9 +16,15 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			this.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			this.transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		
+		// Restrict player position on the x axis
+		float newX = Mathf.Clamp (this.transform.position.x, xMin, xMax);
+		
+		// Update the player position, ensuring they are within the gamespace
+		this.transform.position = new Vector3(newX, this.transform.position.y, this.transform.position.z);
 	}
 }
