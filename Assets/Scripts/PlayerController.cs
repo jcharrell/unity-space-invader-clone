cb3ad8a60cef1aad079a65309c080bce22e3903d
Chris Harrell <chris.harrell@sq1.com>
2017-03-25 19:47:45 -0500

    Add player sprite and add basic movement controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			Vector3 position = this.transform.position;
+			position.x = position.x - speed;
+			this.transform.position = position;
+		} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			Vector3 position = this.transform.position;
+			position.x = position.x + speed;
+			this.transform.position = position;
+		}
+	}
+}
