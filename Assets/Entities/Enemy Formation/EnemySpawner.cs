05120cc3713b1f99290a2df2d193356872d7b9ff
Chris Harrell <chris.harrell@sq1.com>
2017-03-26 15:59:40 -0500

    Move enemey formation left and right within the game space

--- Assets/Entities/Enemy Formation/EnemySpawner.cs
+++ Assets/Entities/Enemy Formation/EnemySpawner.cs
@@ -6,9 +6,23 @@ public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
 	public float width = 10f;
 	public float height = 5f;
+	public float speed = 1f;
+	public float padding = 5f;
+	string direction = "left";
+	float xMin;
+	float xMax;
 	
 	// Use this for initialization
-	void Start () {		
+	void Start () {	
+		// Determine the left and right boundaries of the game space
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		
+		// Define the minimum and maximum x coordinates of the game space
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+			
 		// Loop through each child items (positions) within the EnemyFormation collection
 		foreach(Transform child in transform) {
 			// Spawn enemies on each position
@@ -23,6 +37,20 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(direction == "left") {
+			if(transform.position.x > xMin) {
+				transform.position += Vector3.left * speed * Time.deltaTime;
+			} else {
+				direction = "right";
+			}
+		} else if(direction == "right") {
+			if(transform.position.x < xMax) {
+				transform.position += Vector3.right * speed * Time.deltaTime;
+			} else {
+				direction = "left";
+			}		
+		}
+		
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), transform.position.y, transform.position.z);
 	}
 }
--- Assets/Entities/Enemy Formation/Position.cs
+++ Assets/Entities/Enemy Formation/Position.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Position : MonoBehaviour {
 
 	void OnDrawGizmos() {
+		// Gizmo representing enemy positions within the editor
 		Gizmos.DrawWireSphere(transform.position, 1);
 	}
 }
