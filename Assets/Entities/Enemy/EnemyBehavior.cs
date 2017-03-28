1f631dac52a7f0d579ff03c5cc4924bff5345e79
Chris Harrell <chris.harrell@sq1.com>
2017-03-27 19:56:08 -0500

    Randomize when enemies fire

--- Assets/Entities/Enemy/EnemyBehavior.cs
+++ Assets/Entities/Enemy/EnemyBehavior.cs
@@ -6,10 +6,14 @@ public class EnemyBehavior : MonoBehaviour {
 	public float health = 200f;
 	public GameObject projectilePrefab;
 	public float projectileSpeed = 1f;
-	public float firingRate = 0.2f;
+	public float firingRate = 0.5f;
 	
-	public void Start() {
-		InvokeRepeating("fire", 0.001f, firingRate);
+	public void Update() {
+		float probability = Time.deltaTime * firingRate;
+		
+		if(probability > Random.value) {
+			fire ();
+		}
 	}
 	
 	public void OnTriggerEnter2D(Collider2D collider) {
@@ -25,7 +29,7 @@ public class EnemyBehavior : MonoBehaviour {
 		}
 	}
 	
-	public void fire() {
+	private void fire() {
 		Vector3 projectilePosition = new Vector3(transform.position.x, transform.position.y - 0.7f, transform.position.z);
 		GameObject projectile = Instantiate (projectilePrefab, projectilePosition, Quaternion.identity) as GameObject;
 		projectile.rigidbody2D.velocity = new Vector3(0, -projectileSpeed, 0);
